28f4e0d2c7519bf548854fd3434d6f958360f744
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-08-17 20:20:08 -0400

    Added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Trillium::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/services"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
